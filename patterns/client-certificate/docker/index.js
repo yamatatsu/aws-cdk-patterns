39184772da97f3f0de3331e6a2b3ccc705a7fc38
yamatatsu <the.kinnikuman@gmail.com>
2020-04-24 20:22:39 +0900

    express

--- patterns/client-certificate/docker/index.js
+++ patterns/client-certificate/docker/index.js
@@ -2,8 +2,42 @@ const https = require("https")
 const http = require("http")
 const fs = require("fs")
 const url = require("url")
+const express = require("express")
 const AWS = require("aws-sdk")
 
+// Getパラメータで渡されるOrg
+const getOrgByParam = (req) => url.parse(req.url, true).query.o
+// 実際の接続時に利用された証明書のOrg
+const getOrgByCert = (res) => res.connection.getPeerCertificate().subject?.O
+const getCertText = (orgByParam, orgByCert) =>
+  `Param:${orgByParam}\nOrg  :${orgByCert}\n`
+
+const clientCertificateMiddleware = (req, res, next) => {
+  const orgByParam = getOrgByParam(req)
+  const orgByCert = getOrgByCert(res)
+
+  console.log({ orgByParam, orgByCert })
+
+  // 一致しない場合406
+  if (!orgByParam || !orgByCert || orgByParam !== orgByCert) {
+    res.statusCode = 406
+    res.end()
+  } else {
+    next()
+  }
+}
+
+const app = express()
+app.use(clientCertificateMiddleware)
+app.get("/", (req, res) => {
+  const orgByParam = getOrgByParam(req)
+  const orgByCert = getOrgByCert(res)
+  res.send(
+    `Hello, world. ${JSON.stringify({ orgByParam, orgByCert }, null, 2)}`,
+  )
+})
+
+// ssl server
 const options = {
   key: fs.readFileSync("./keys/server-key.pem"),
   cert: fs.readFileSync("./keys/server-crt.pem"),
@@ -11,33 +45,11 @@ const options = {
   requestCert: true, // クライアント認証（true:する, false:しない）
   rejectUnauthorized: true, // 認証失敗時に破棄（true:する, false:しない）
 }
-
-const handler = (req, res) => {
-  // Getパラメータで渡されるOrg
-  const orgByParam = url.parse(req.url, true).query.o
-  // 実際の接続時に利用された証明書のOrg
-  const orgByCert = res.connection.getPeerCertificate().subject?.O
-
-  const outText = `Param:${orgByParam}\nOrg  :${orgByCert}\n`
-  console.log(outText)
-
-  // 一致しない場合406
-  if (!orgByParam || !orgByCert || orgByParam !== orgByCert) {
-    res.statusCode = 406
-    res.end()
-    return
-  }
-
-  res.setHeader("Content-Type", "text/plain")
-  res.end(`Hello, world\n${outText}\n`)
-}
-
-// ssl server
-https.createServer(options, handler).listen(443, () => {
+https.createServer(options, app).listen(443, () => {
   console.log(`Server is listening https!!`)
 })
 
 // non ssl server
-http.createServer(handler).listen(3000, () => {
+http.createServer(app).listen(3000, () => {
   console.log(`Server is listening http!!`)
 })
