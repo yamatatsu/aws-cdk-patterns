a7187f85f5d951ff0b9b32a6a7a970eec946f835
yamatatsu <the.kinnikuman@gmail.com>
2020-04-27 14:52:38 +0900

    use secretManager only

--- patterns/client-certificate/docker/index.js
+++ patterns/client-certificate/docker/index.js
@@ -40,38 +40,31 @@ app.get("/", (req, res) => {
   )
 })
 
+// ssl server
 const secretPromise = secretsManager
   .getSecretValue({ SecretId: SECRET_ID })
   .promise()
-  .then((data) => {
+  .then((secretData) => {
     console.info("Success `secretsManager.getSecretValue`")
-    return data
-  })
 
-const acmPromise = acm
-  .getCertificate({ CertificateArn: SERVER_CERTIFICATE_ARN })
-  .promise()
-  .then((data) => {
-    console.info("Success `acm.getCertificate`")
-    return data
-  })
-
-// ssl server
-Promise.all([secretPromise, acmPromise]).then(
-  ([secretData, certificateData]) => {
     const secret = JSON.parse(secretData.SecretString)
+    const [SERVER_KEY, SERVER_CRT, CA_CRT] = [
+      "SERVER_KEY",
+      "SERVER_CRT",
+      "CA_CRT",
+    ].map((name) => secret[name].replace(/\\n/g, "\n"))
+
     const options = {
-      key: secret.SERVER_KEY.replace(/\\n/g, "\n"),
-      cert: certificateData.Certificate,
-      ca: secret.CA_CRT.replace(/\\n/g, "\n"), // クライアント証明書のCA証明書
+      key: SERVER_KEY,
+      cert: SERVER_CRT,
+      ca: CA_CRT, // クライアント証明書のCA証明書
       requestCert: true, // クライアント認証（true:する, false:しない）
       rejectUnauthorized: true, // 認証失敗時に破棄（true:する, false:しない）
     }
     https.createServer(options, app).listen(443, () => {
       console.log(`Server is listening https!!`)
     })
-  },
-)
+  })
 
 // non ssl server
 http.createServer(app).listen(3000, () => {
