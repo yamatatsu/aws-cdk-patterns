a0f31b4fe37dd31242096a6d45f1bc09e59c8ce4
yamatatsu <the.kinnikuman@gmail.com>
2020-04-27 12:21:48 +0900

    use aws-sddk

--- patterns/client-certificate/docker/index.js
+++ patterns/client-certificate/docker/index.js
@@ -5,12 +5,15 @@ const url = require("url")
 const express = require("express")
 const AWS = require("aws-sdk")
 
+const secretsManager = new AWS.SecretsManager({ region: "ap-northeast-1" })
+const acm = new AWS.ACM({ region: "ap-northeast-1" })
+
+const { SECRET_ID, SERVER_CERTIFICATE_ARN } = process.env
+
 // Getパラメータで渡されるOrg
 const getOrgByParam = (req) => url.parse(req.url, true).query.o
 // 実際の接続時に利用された証明書のOrg
 const getOrgByCert = (res) => res.connection.getPeerCertificate().subject?.O
-const getCertText = (orgByParam, orgByCert) =>
-  `Param:${orgByParam}\nOrg  :${orgByCert}\n`
 
 const clientCertificateMiddleware = (req, res, next) => {
   const orgByParam = getOrgByParam(req)
@@ -37,17 +40,38 @@ app.get("/", (req, res) => {
   )
 })
 
+const secretPromise = secretsManager
+  .getSecretValue({ SecretId: SECRET_ID })
+  .promise()
+  .then((data) => {
+    console.info("Success `secretsManager.getSecretValue`")
+    return data
+  })
+
+const acmPromise = acm
+  .getCertificate({ CertificateArn: SERVER_CERTIFICATE_ARN })
+  .promise()
+  .then((data) => {
+    console.info("Success `acm.getCertificate`")
+    return data
+  })
+
 // ssl server
-const options = {
-  key: fs.readFileSync("./keys/server-key.pem"),
-  cert: fs.readFileSync("./keys/server-crt.pem"),
-  ca: fs.readFileSync("./keys/ca-crt.pem"), // クライアント証明書
-  requestCert: true, // クライアント認証（true:する, false:しない）
-  rejectUnauthorized: true, // 認証失敗時に破棄（true:する, false:しない）
-}
-https.createServer(options, app).listen(443, () => {
-  console.log(`Server is listening https!!`)
-})
+Promise.all([secretPromise, acmPromise]).then(
+  ([secretData, certificateData]) => {
+    const secret = JSON.parse(secretData.SecretString)
+    const options = {
+      key: secret.SERVER_KEY.replace(/\\n/g, "\n"),
+      cert: certificateData.Certificate,
+      ca: secret.CA_CRT.replace(/\\n/g, "\n"), // クライアント証明書のCA証明書
+      requestCert: true, // クライアント認証（true:する, false:しない）
+      rejectUnauthorized: true, // 認証失敗時に破棄（true:する, false:しない）
+    }
+    https.createServer(options, app).listen(443, () => {
+      console.log(`Server is listening https!!`)
+    })
+  },
+)
 
 // non ssl server
 http.createServer(app).listen(3000, () => {
