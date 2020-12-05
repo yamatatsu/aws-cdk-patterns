4dcfd6bfac5b06ee0707bc05d9a7f29ed0d3c2db
yamatatsu <the.kinnikuman@gmail.com>
2020-12-05 23:07:54 +0900

    cis-2.7 and fsbp-cloudtrail-2

--- patterns/security/stacks/CloudTrail.ts
+++ patterns/security/stacks/CloudTrail.ts
@@ -1,5 +1,7 @@
 import * as cdk from "@aws-cdk/core"
 import * as logs from "@aws-cdk/aws-logs"
+import * as kms from "@aws-cdk/aws-kms"
+import * as iam from "@aws-cdk/aws-iam"
 import * as cloudtrail from "@aws-cdk/aws-cloudtrail"
 
 export class CloudTrail extends cdk.Stack {
@@ -12,6 +14,18 @@ export class CloudTrail extends cdk.Stack {
       retention: logs.RetentionDays.THREE_MONTHS,
     })
 
+    const key = new kms.Key(this, "KmsKey", {
+      description: "For CloudTrail Storage Encryption",
+      alias: "for-cloud-trail-storage-encryption",
+      // For private AWS account
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    })
+
+    // TODO: It will be fixed by https://github.com/aws/aws-cdk/issues/3822
+    key.grantEncrypt({
+      grantPrincipal: new iam.ServicePrincipal("cloudtrail.amazonaws.com"),
+    })
+
     new cloudtrail.Trail(this, "CloudTrail", {
       includeGlobalServiceEvents: true,
       isMultiRegionTrail: true,
@@ -19,6 +33,10 @@ export class CloudTrail extends cdk.Stack {
       managementEvents: cloudtrail.ReadWriteType.ALL,
       sendToCloudWatchLogs: true,
       cloudWatchLogGroup: logGroup,
+      // https://docs.aws.amazon.com/securityhub/latest/userguide/securityhub-cis-controls.html#cis-2.7-remediation
+      // and
+      // https://docs.aws.amazon.com/securityhub/latest/userguide/securityhub-standards-fsbp-controls.html#cloudtrail-2-remediation
+      encryptionKey: key,
     })
 
     this.logGroup = logGroup
