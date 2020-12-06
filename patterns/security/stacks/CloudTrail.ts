e163d9b3f2d9d684dfa8e9f0088b87206eae6fe2
yamatatsu <the.kinnikuman@gmail.com>
2020-12-06 23:44:42 +0900

    fsbp-s3.5g g d

--- patterns/security/stacks/CloudTrail.ts
+++ patterns/security/stacks/CloudTrail.ts
@@ -2,7 +2,24 @@ import * as cdk from "@aws-cdk/core"
 import * as logs from "@aws-cdk/aws-logs"
 import * as kms from "@aws-cdk/aws-kms"
 import * as iam from "@aws-cdk/aws-iam"
+import * as s3 from "@aws-cdk/aws-s3"
 import * as cloudtrail from "@aws-cdk/aws-cloudtrail"
+
+const ssslOnlyPolicyStatement = new iam.PolicyStatement({
+  sid: "AllowSSLRequestsOnly",
+  actions: ["s3:*"],
+  effect: iam.Effect.DENY,
+  resources: [
+    "arn:aws:s3:::awsexamplebucket",
+    "arn:aws:s3:::awsexamplebucket/*",
+  ],
+  conditions: {
+    Bool: {
+      "aws:SecureTransport": "false",
+    },
+  },
+  principals: [new iam.AnyPrincipal()],
+})
 
 export class CloudTrail extends cdk.Stack {
   public readonly logGroup: logs.ILogGroup
@@ -28,6 +45,31 @@ export class CloudTrail extends cdk.Stack {
       grantPrincipal: new iam.ServicePrincipal("cloudtrail.amazonaws.com"),
     })
 
+    const accessLogBucket = new s3.Bucket(
+      this,
+      "CloudTrailBucketAccessLogBucket",
+      {
+        encryptionKey: key,
+        // For private AWS account
+        removalPolicy: cdk.RemovalPolicy.DESTROY,
+      },
+    )
+    // https://docs.aws.amazon.com/securityhub/latest/userguide/securityhub-standards-fsbp-controls.html#s3-5-remediation
+    accessLogBucket.addToResourcePolicy(ssslOnlyPolicyStatement)
+
+    const bucket = new s3.Bucket(this, "CloudTrailBucket", {
+      // https://docs.aws.amazon.com/securityhub/latest/userguide/securityhub-cis-controls.html#cis-2.7-remediation
+      // and
+      // https://docs.aws.amazon.com/securityhub/latest/userguide/securityhub-standards-fsbp-controls.html#cloudtrail-2-remediation
+      encryptionKey: key,
+      // https://docs.aws.amazon.com/securityhub/latest/userguide/securityhub-cis-controls.html#cis-2.6-remediation
+      serverAccessLogsBucket: accessLogBucket,
+      // For private AWS account
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    })
+    // https://docs.aws.amazon.com/securityhub/latest/userguide/securityhub-standards-fsbp-controls.html#s3-5-remediation
+    bucket.addToResourcePolicy(ssslOnlyPolicyStatement)
+
     new cloudtrail.Trail(this, "CloudTrail", {
       includeGlobalServiceEvents: true,
       isMultiRegionTrail: true,
@@ -39,6 +81,7 @@ export class CloudTrail extends cdk.Stack {
       // and
       // https://docs.aws.amazon.com/securityhub/latest/userguide/securityhub-standards-fsbp-controls.html#cloudtrail-2-remediation
       encryptionKey: key,
+      bucket,
     })
 
     this.logGroup = logGroup
