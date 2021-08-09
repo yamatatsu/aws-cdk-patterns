b222354db754a800b3372116cc46d632c04c886a
yamatatsu <the.kinnikuman@gmail.com>
2021-08-09 21:17:54 +0900

    simplify solo

--- patterns/iam-mania-solo/stacks.ts
+++ patterns/iam-mania-solo/stacks.ts
@@ -4,32 +4,10 @@ export class IamManiaSolo extends Stack {
   constructor(parent: App, id: string, props?: StackProps) {
     super(parent, id, props)
 
-    const mfaForcePolicy = new aws_iam.ManagedPolicy(this, "MFAForcePolicy", {
-      managedPolicyName: "MFAForcePolicy",
-      statements: [
-        // [DENY]MFA必須化
-        new aws_iam.PolicyStatement({
-          effect: aws_iam.Effect.DENY,
-          notActions: ["iam:*"],
-          resources: ["*"],
-          conditions: {
-            BoolIfExists: { "aws:MultiFactorAuthPresent": "false" },
-          },
-        }),
-        // [ALLOW]全ユーザーのリスト表示
-        new aws_iam.PolicyStatement({
-          effect: aws_iam.Effect.ALLOW,
-          actions: ["iam:ListUsers", "iam:ListVirtualMFADevices"],
-          resources: ["*"],
-        }),
-      ],
-    })
-
     const adminGroup = new aws_iam.Group(this, "AdminGroup", {
       groupName: "AdminGroup",
       managedPolicies: [
         aws_iam.ManagedPolicy.fromAwsManagedPolicyName("ReadOnlyAccess"),
-        mfaForcePolicy,
       ],
     })
 
